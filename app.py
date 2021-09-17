ec2e74c0c33623d22da1a607ced6339c7c625d5b
crishernandezmaps <cris@crishernandez.co>
2021-09-17 15:02:09 -0300

    form

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-from flask import Flask, render_template, request, session, redirect, url_for, Blueprint, send_file
+from flask import Flask, render_template, request, session, redirect, url_for
 import os
-from welokat_near import welokat_near
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        address = request.form['address']
-        bot = welokat_near(address)
-        data = bot.wnear()  
-        print(data)  
-        if type(data) == str:    
-            return render_template('sorry.html', data=data)
-        if type(data) == dict:
-            return render_template('result.html', data=data['data'], latlon=data['latlon'], res=data['res'])
-        else:
-            pass
     return render_template('index.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')    
 
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
-"""
-TODO:
-- Sorry page
-- Check maximun requests by minute
-- Limit use by IP
-""" 
