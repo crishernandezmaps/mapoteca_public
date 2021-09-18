307f1cd3b78c8cbe8e00283328118d4fc0e77426
crishernandezmaps <cris@crishernandez.co>
2021-09-18 00:52:42 -0300

    first mvp

--- app.py
+++ app.py
@@ -1,6 +1,88 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
 import pandas as pd
+import json
+from keplergl import KeplerGl
+
+# FOR THE MAP
+conf_scl = json.load(open('data/config_santiago.json',))
+conf_lim = json.load(open('data/config_lima.json',))
+conf_bog = json.load(open('data/config_bogota.json',))
+conf_mex = json.load(open('data/config_mexico.json',))
+
+txt_html = b'''<meta charset="utf-8">
+            <title>Mapoteca</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="Mapoteca Public">
+            <meta name="keywords" content="Mapoteca, Maps, Geospatial, Insights, Mobility">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap" rel="stylesheet" />            
+            <style>
+                @import url('https://fonts.googleapis.com/css?family=Montserrat:600&display=swap');
+                div.nav-inside {
+                    position: absolute;
+                    z-index: 30;
+                    top: -1%;
+                    right: 3%;
+                    margin-bottom: 5%;
+                }
+                div.nav-inside > ul > li  {
+                    display: inline-block;
+                    zoom: 1;
+                    display: inline;
+                }
+                div.nav-inside > ul > li > a {
+                    color: rgb(255, 255, 255);
+                    font-size: 20px;
+                    line-height: 1.71429;
+                    font-weight: 400;
+                    font-size: 0.8em;
+                    text-decoration: none;
+                }
+                .map-control {
+                    margin-top: 150px !important;
+                }
+                .geocoder-panel {
+                    top: 166px !important;
+                    right: 60px !important;
+                }
+                button {
+                    margin: 50px;
+                    font-family: inherit;
+                }                
+                .fill {
+                    z-index: 100; 
+                    position: absolute;
+                    right: 0%;
+                    font-size: 20px;
+                    font-weight: 200;
+                    font-family: 'Montserrat', sans-serif;
+                    letter-spacing: 1px;
+                    padding: 13px 50px 13px;
+                    outline: 0;
+                    border: 1px solid #0DFF92;
+                    cursor: pointer;
+                    background-color: rgba(0, 0, 0, 0);
+                }
+
+                .fill::after {
+                    content: "";
+                    background-color: #0DFF92;
+                    width: 100%;
+                    z-index: -1;
+                    position: absolute;
+                    height: 100%;
+                    top: 7px;
+                    left: 7px;
+                    transition: 0.2s;
+                }
+
+                .fill:hover::after {
+                    top: 0px;
+                    left: 0px;
+                }                
+            </style>
+            <a href="./"><button type="button" class="fill">Volver</button></a>'''
 
 app = Flask(__name__)
 
@@ -18,9 +100,9 @@ def fscl():
         print(category)    
         m = df[[category, 'lat', 'lon']]
         m.sort_values(by=[category], ascending=False, inplace=True)
-        to_map = m.loc[m[category] >= 0.8]
-        print(df.shape, m.shape, to_map.shape)
-        return render_template('index.html')
+        map_1 = KeplerGl(config=conf_scl, data={'data': m}, options={"readOnly": True, "centerMap": True})
+        r = txt_html + map_1._repr_html_()
+        return r        
     return render_template('fscl.html')    
 
 @app.route('/flim', methods=['GET', 'POST'])
@@ -33,9 +115,9 @@ def flim():
         print(category)    
         m = df[[category, 'lat', 'lon']]
         m.sort_values(by=[category], ascending=False, inplace=True)
-        to_map = m.loc[m[category] >= 0.8]
-        print(df.shape, m.shape, to_map.shape)
-        return render_template('index.html')
+        map_1 = KeplerGl(data={'data': m}, config=conf_lim, options={"readOnly": True, "centerMap": True})
+        r = txt_html + map_1._repr_html_()
+        return r        
     return render_template('flim.html') 
 
 @app.route('/fbog', methods=['GET', 'POST'])
@@ -48,9 +130,9 @@ def fbog():
         print(category)    
         m = df[[category, 'lat', 'lon']]
         m.sort_values(by=[category], ascending=False, inplace=True)
-        to_map = m.loc[m[category] >= 0.8]
-        print(df.shape, m.shape, to_map.shape)
-        return render_template('index.html')
+        map_1 = KeplerGl(data={'data': m}, config=conf_bog, options={"readOnly": True, "centerMap": True})
+        r = txt_html + map_1._repr_html_()
+        return r  
     return render_template('fbog.html')   
 
 @app.route('/fmex', methods=['GET', 'POST'])
@@ -63,9 +145,9 @@ def fmex():
         print(category)    
         m = df[[category, 'lat', 'lon']]
         m.sort_values(by=[category], ascending=False, inplace=True)
-        to_map = m.loc[m[category] >= 0.8]
-        print(df.shape, m.shape, to_map.shape)
-        return render_template('index.html')
+        map_1 = KeplerGl(data={'data': m}, config=conf_mex, options={"readOnly": True, "centerMap": True})
+        r = txt_html + map_1._repr_html_()
+        return r
     return render_template('fmex.html')    
 
 if __name__ == "__main__":
